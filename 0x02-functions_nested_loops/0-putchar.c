43ae501eba445be350d15554f52f544c9e264686
thabimt <shazzyzuma@yahoo.com>
2023-10-10 16:15:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include "main.h"
 /**
-*main - print putchar
+*main - print _putchar
 *Return:(0) sucess
 *
 */
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include< stdio.h>
+/**
+*main - I sometimes suffer from insomnia
+*Return: (0) success
+*
+*/
+int main(void)
+{
+charletter;
+
+for (letter= 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+
+_putchar('\n');
+}
