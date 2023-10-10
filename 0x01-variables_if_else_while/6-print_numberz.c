1bf2aca1d72f9ff785cc06f11fafc20e4142f515
thabimt <shazzyzuma@yahoo.com>
2023-10-09 20:20:12 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
- * main - print decimal without char
- *
- * return: (0) always on success
- *
- */
+*main - print decimal without char
+*
+*Return: (0) always on success
+*
+*/
 int main(void)
 {
-	int d = '0';
+int d = '0';
 
-	while (d <= '9')
-	{
-		putchar(d);
-		d++;
-	}
-	putchar('\n');
+while (d <= '9')
+{
+putchar(d);
+d++;
+}
+putchar('\n');
 
-	return (0);
+return (0);
 }
