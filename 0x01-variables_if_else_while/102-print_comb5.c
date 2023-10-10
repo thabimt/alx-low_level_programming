0754be78bf5e9caee7b647647ba0f164fdccda0d
thabimt <shazzyzuma@yahoo.com>
2023-10-09 20:54:40 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+*main - sofstware
+*Return:(0) sucess
+*
+*/
+int main(void)
+{
+int i, j;
+
+for (i = 0; i < 100; i++)
+{
+for (j = i; j < 100; j++)
+{
+putchar('0' + i / 10);
+putchar('0' + i % 10);
+putchar(' ');
+putchar('0' + j / 10);
+putchar('0' + j % 10);
+
+if (i != 99 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
