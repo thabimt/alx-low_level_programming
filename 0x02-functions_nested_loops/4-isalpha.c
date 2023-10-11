b11baa21f6b6d7ac015f315e770c14bc0caa7f32
thabimt <shazzyzuma@yahoo.com>
2023-10-11 08:43:59 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,27 +1,10 @@
 #include "main.h"
 /**
-*_isalpha - return 1 when c is cap or low
-*
-*Description:use _putchar to print
-*
-*@c: collect the character
-*return: 1 if c is low or cap otherwise 0
-*
-*/
-int _islower(int c)
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
 {
-	char lower, upper;
-	int letter = 0;
-
-	for (lower = 'a'; lower <= 'z'; lower++)
-	{
-		for (upper = 'A'; upper <= 'z'; upper++)
-		{
-			if (lower == 'c' || upper == 'C')
-			{
-				letter = 1;
-			}
-		}
-	}
-	return (letter);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
