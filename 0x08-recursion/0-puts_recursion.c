ae12e278db31c5154716672004aadfe60c533690
thabimt <shazzyzuma@yahoo.com>
2023-10-25 13:42:45 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts_recursion - function like puts();
  * @s: input
@@ -9,7 +10,7 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursions(s + 1);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
