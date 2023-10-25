7339f28c2728a59ae3f9a44e61faff4ad8469e49
thabimt <shazzyzuma@yahoo.com>
2023-10-25 15:41:11 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _puts_recursion - function like puts();
  * @s: input
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
