36efdaa1e985edb9e307135cf9762209b5ed5c46
thabimt <shazzyzuma@yahoo.com>
2023-10-27 17:47:13 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdion.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: number of command line arg...
+ * @argv: the array that contains the command line..
+ * Return: (0) when successful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
