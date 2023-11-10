e75418f358ba2dbe89404676368869c235053887
thabimt <shazzyzuma@yahoo.com>
2023-11-09 19:04:24 -0800

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,19 +1,18 @@
 #include "variadic_functions.h"
 /**
- * print_all - print any argument provided to
- * function
+ * print_all - print any argument provided
  * @format: the argument specifier
  *
  * Return: any argument given based on format specifier
  */
 void print_all(const char * const format, ...)
 {
-	int i, check_stat;
+	int i, check_stat; /* declare variables and va_arg datatype */
 
 	char *str;
 	va_list spc;
 
-	va_start(spc, format);
+	va_start(spc, format); /* initialize var arguments */
 
 	i = 0;
 	while (format != NULL && format[i] != '\0')
@@ -22,7 +21,7 @@ void print_all(const char * const format, ...)
 		{
 			case 'i':
 				printf("%d", va_arg(spc, int));
-				check_stat = 0;
+				check_stat = 0; /* check if condition has been met */
 				break;
 			case 'f':
 				printf("%f", va_arg(spc, double));
@@ -42,10 +41,10 @@ void print_all(const char * const format, ...)
 				check_stat = 1;
 				break;
 		}
-		if (format[i + 1] != '\0' && check_stat == 0)
+		if (format[i + 1] != '\0' && check_stat == 0) /* if NOT NULL */
 			printf(", ");
-		i++;
+		i++; /* update step of iter var */
 	}
 	printf("\n");
-	va_end(spc);
+	va_end(spc); /* end traversal */
 }
