e49434e337766bd571986e923a1bd8a40dccc442
thabimt <shazzyzuma@yahoo.com>
2023-11-17 03:21:50 -0800

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,47 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
-int len(const char *str);
+
 /**
- * add_node - add a node to the begining of the list
- * @head: pointer to the head of the list
- * @str: string to be inserted to the first position
- * Return: pointer to the first node (newly assigne one)
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
+ *
+ * Return: the address of the new element, or NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new;
+	unsigned int len = 0;
 
-	if (head == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+	while (str[len])
+	len++;
 
-	if (*head == NULL)
-		new_node->next = NULL;
-	else
-	{
-		new_node->next = *head;
-		new_node->str = strdup(str);
-		new_node->len = len(str);
-		*head = new_node;
-	}
+	new = malloc(sizeof(list_t));
+	if (!new)
+	return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
 	return (*head);
-/**
- * len - get the length of str (string)
- * @str: the string to get its length
- * Return: length of the strength
- */
-int len(const char *str)
-{
-	int i;
-
-	if (str == NULL)
-		return (0);
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
 }
